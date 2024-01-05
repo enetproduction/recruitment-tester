cde2219d3fdcf7a7d3a29871ac5e99676ec8628c
Piotr Badełek <piotr.badelek@enp.pl>
2024-01-05 12:55:43 +0100

    modify task description

--- cypress/e2e/tasks/task-4/test_4.cy.ts
+++ cypress/e2e/tasks/task-4/test_4.cy.ts
@@ -2,7 +2,7 @@
 1. Napisz test automatyczny, który realizuje poniższe kroki testowe:
 2. Przejdź do strony logowania
 3. Zaloguj się do aplikacji
-4. Pobierz wszystkie karty produktów z dostępnego listingu
+4. Pobierz wszystkie karty produktów z listy produktowej
 5. Wybierz losowo jeden z produktów
 6. Dodaj go do koszyka
 7. Dokonaj weryfikacji, czy licznik w koszyku się zwiększa

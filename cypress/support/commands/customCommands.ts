1c466fc3ea722204f4f3fa7efc6283c6fe366d3e
Piotr Badełek <piotr.badelek@enp.pl>
2024-02-21 10:31:20 +0100

    fix lint problems and extensions

--- cypress/support/commands/customCommands.ts
+++ cypress/support/commands/customCommands.ts
@@ -1,10 +1,12 @@
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       openPage: typeof openPage;
     }
   }
 }
+
 export const openPage = (pageUrl: string): void => {
   cy.visit(pageUrl);
 };
